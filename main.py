85583df9e132fa7d4b10e881909450dec8e58911
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-07-26 13:30:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import subprocess
+import re
+
+result = subprocess.run(["sm", "--status"], capture_output=True, encoding="UTF-8")
+stdout = result.stdout
+services = map(lambda x: x, re.findall("\|\s+(?P<service>[A-Z_]+)\s+\|", stdout))
+result = subprocess.run(["sm", "--stop"] + list(services))
