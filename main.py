25400e3dff36a0ee456ae549823036a8fad89c9c
Daniel Naylor <93977310+drnaylor-oc@users.noreply.github.com>
2022-08-02 16:42:13 +0100

    A little more refinement

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import re
 
 print("Searching for services to stop...")
 result = subprocess.run(["sm", "--status"], capture_output=True, encoding="UTF-8")
+
+# When there is a group in the regex, findall will return a list of tuples of the groups,
+# in this case, because there is only one group, it would be a tuple of 1, so it's just
+# a list of matches
 services = map(lambda x: x, re.findall(r"\|\s+(?P<service>[A-Z_]+)\s+\|", result.stdout))
 
 listOfServices = list(services)
